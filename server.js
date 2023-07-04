7b161f1894ba3f9bc93bb2f1c1b6069c9ea82e34
Latrell Page <pagelatrell14@gmail.com>
2023-07-03 21:38:07 -0500

    Moved files to root directory and creating paths for get and post routes

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3001; // Choose a port number for your server
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let data = require('./db/db.json')
+let notes = [];
+
+// Route for retrieving all notes
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+// Route for saving a new note
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  data.push(newNote);
+  fs.writeFile("./db/db.json", data);
+});
+
+// Route for deleting a note
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  notes = notes.filter((note) => note.id !== noteId);
+  res.sendStatus(204);
+});
+
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + '/public'));
+
+// Route handler for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(__dirname + '/public/notes.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+
+
